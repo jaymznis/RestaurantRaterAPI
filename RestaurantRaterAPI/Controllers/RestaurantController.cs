5b304f5df44672bf43b2b8355699b7542e986bf6
James Campbell <jaymznis@gmail.com>
2021-05-25 13:13:52 -0400

    added Restaurant class and controller added migrations

---
+++ RestaurantRaterAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,33 @@
+﻿using RestaurantRaterAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRaterAPI.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private RestaurantDbContext _context = new RestaurantDbContext();
+
+        public async Task<IHttpActionResult> PostReastaurant(Restaurant model)
+        {
+            if (model == null)
+            {
+                return BadRequest("your request boday cannot be empty");
+            }
+            //checks if every proprerty that is required is included from Restaurant class (model)
+            if (ModelState.IsValid)
+            {
+                _context.Restaurants.Add(model);
+                await _context.SaveChangesAsync();
+
+                return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
---
+++ RestaurantRaterAPI/Models/Restaurant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRaterAPI.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+        
+        [Required]
+        public double Rating { get; set; }
+
+        public bool IsRecommended => Rating > 3.5;
+       // {
+           // get
+            //{ write it like this
+               // return Rating > 3.5;
+               //or this
+                //bool isRecommended = Rating > 3.5;
+                //return isRecommended;
+           // }
+       // }
+    }
+}
